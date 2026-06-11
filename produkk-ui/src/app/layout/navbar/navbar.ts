3625b1ff3cdfac855cfcb52d4df2ce1b1061c16d
jakopo-webdev <jakopozhegrova.business@gmail.com>
2026-06-11 17:37:21 +0200

    ui/ux: update navbar to conditionally display logout button only if the user is authenticated

--- produkk-ui/src/app/layout/navbar/navbar.ts
+++ produkk-ui/src/app/layout/navbar/navbar.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf],
   templateUrl: './navbar.html',
 })
 export class NavbarComponent {
