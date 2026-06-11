df2b809eedbf044c4929a42d1fb7a251fa0a7029
jakopo-webdev <jakopozhegrova.business@gmail.com>
2026-06-11 11:41:15 +0200

    feat: add footer component and put the links there

--- produkk-ui/src/app/app.ts
+++ produkk-ui/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CookieBannerComponent } from './core/components/cookie-banner';
+import { FooterComponent } from './layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CookieBannerComponent],
+  imports: [RouterOutlet, CookieBannerComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- produkk-ui/src/app/layout/navbar/navbar.ts
+++ produkk-ui/src/app/layout/navbar/navbar.ts
@@ -1,11 +1,9 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
   templateUrl: './navbar.html',
 })
 export class NavbarComponent {
