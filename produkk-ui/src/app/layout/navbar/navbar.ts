af9816619b8e46310f1231c18e2f1d8ff0b36853
jakopo-webdev <jakopozhegrova.business@gmail.com>
2026-06-11 11:53:00 +0200

    ui/ux: wrap logo in a link that redirects to "/"

--- produkk-ui/src/app/layout/navbar/navbar.ts
+++ produkk-ui/src/app/layout/navbar/navbar.ts
@@ -1,9 +1,11 @@
 import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
+  imports: [RouterLink],
   templateUrl: './navbar.html',
 })
 export class NavbarComponent {
